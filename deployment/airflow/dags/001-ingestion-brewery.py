35bda24e5681d1932ff8188c09a4e4964e891570
João Pedro Campos Faria <ocamposfaria@gmail.com>
2025-04-08 11:23:04 -0300

    Tentando corrigir Spark submit

--- deployment/airflow/dags/001-ingestion-brewery.py
+++ deployment/airflow/dags/001-ingestion-brewery.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from classes.breweries import extract_breweries, extract_metadata
+from airflow.operators.bash import BashOperator
 
 default_args = {
     'owner': 'airflow',
@@ -29,5 +30,11 @@ with DAG(
         python_callable=extract_metadata
     )
 
+    task_spark_transformation = BashOperator(
+        task_id='run_spark_transformation',
+        bash_command='docker exec spark spark-submit --conf spark.jars.ivy=/tmp/.ivy2 /opt/spark/test_job.py'
+    )
+
     task_extract_breweries
     task_extract_metadata
+    task_spark_transformation
