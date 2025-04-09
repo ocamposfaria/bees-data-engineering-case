841dc61b9eb8d5668461890978dd8fa2fcb7e38b
João Pedro Campos Faria <ocamposfaria@gmail.com>
2025-04-09 20:59:32 -0300

    Minor fixes

--- deployment/spark/apps/test_job.py
+++ deployment/spark/apps/test_job.py
@@ -1,13 +1,20 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("WordCount").getOrCreate()
+# Criando a SparkSession com configs do MinIO
+spark = SparkSession.builder \
+    .appName("Read from MinIO S3A") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+    .config("spark.hadoop.fs.s3a.access.key", "ROOTUSER") \
+    .config("spark.hadoop.fs.s3a.secret.key", "CHANGEME123") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.defaultFS", "s3a://datalake") \
+    .getOrCreate()
 
-data = ["olá mundo", "mundo spark", "hello world", "hello hello"]
-df = spark.createDataFrame(data, "string").toDF("linha")
+# Lê o arquivo Parquet do MinIO
+df = spark.read.parquet("s3a://datalake/teste_parquet")
+df.show()
 
-words = df.selectExpr("explode(split(linha, ' ')) as palavra")
-contagem = words.groupBy("palavra").count()
-
-contagem.show()
-
-spark.stop()
+# Lê o arquivo JSON do MinIO
+df_json = spark.read.json("s3a://datalake/bronze/breweries.json")
+df_json.show(truncate=False)
