1684c1da65f7a1576e7e654b8d09c5cf7e057462
João Pedro Campos Faria <ocamposfaria@gmail.com>
2025-04-11 20:47:46 -0300

    Construindo task de metadata

--- deployment/airflow/dags/elt_001_breweries.py
+++ deployment/airflow/dags/elt_001_breweries.py
@@ -2,8 +2,18 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
-from tasks.tasks_elt_001_breweries import bronze_extract_breweries, bronze_extract_metadata
+from tasks.tasks_elt_001_breweries import bronze_extract_breweries, bronze_extract_metadata, ipynb_command
+import os
+import json
 
+def read_and_log_gold_file():
+    file_path = "/opt/airflow/spark/notebooks/3_gold/gold_001_breweries.json"  # Caminho atualizado para o arquivo JSON
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            content = json.load(file)  # Carregar o conteúdo do arquivo JSON
+            print(f"Conteúdo do arquivo {file_path}:\n{json.dumps(content, indent=2)}")  # Imprime o JSON de forma legível
+    else:
+        print(f"Arquivo {file_path} não encontrado.")
 
 with DAG(
     dag_id="elt_001_breweries",
@@ -25,13 +35,17 @@ with DAG(
 
     task_silver_transform_breweries = BashOperator(
         task_id="silver_transform_breweries",
-        bash_command=(
-            "docker exec spark-spark-master-1 bash -c \""
-            "jupyter nbconvert --to script /opt/spark-apps/2_silver/silver_001_breweries.ipynb && "
-            "/opt/spark/bin/spark-submit "
-            "--master spark://spark-spark-master-1:7077 "
-            "local:///opt/spark-apps/2_silver/silver_001_breweries.py\""
-        )
+        bash_command=ipynb_command("silver_001_breweries")
     )
 
-    [task_bronze_extract_breweries, task_bronze_extract_metadata]>>task_silver_transform_breweries
+    task_gold_transform_breweries = BashOperator(
+        task_id="gold_transform_breweries",
+        bash_command=ipynb_command("gold_001_breweries")
+    )
+
+    task_read_and_log_gold_file = PythonOperator(
+        task_id="read_and_log_gold_file",
+        python_callable=read_and_log_gold_file
+    )
+
+    [task_bronze_extract_breweries, task_bronze_extract_metadata] >> task_silver_transform_breweries >> task_gold_transform_breweries >> task_read_and_log_gold_file
