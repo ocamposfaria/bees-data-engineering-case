da7498a30deb16ffbf86125c784a1613185a5d70
João Pedro Campos Faria <ocamposfaria@gmail.com>
2025-04-12 20:09:51 -0300

    Adicionando envio de e-mail em caso de falha

--- airflow/dags/elt_001_breweries.py
+++ airflow/dags/elt_001_breweries.py
@@ -2,7 +2,19 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
-from tasks.tasks_elt_001_breweries import bronze_extract_breweries, bronze_extract_metadata, ipynb_command, publish_metadata
+from tasks.tasks_elt_001_breweries import (
+    bronze_extract_breweries,
+    bronze_extract_metadata,
+    ipynb_command,
+    publish_metadata
+)
+
+default_args = {
+    "email": ["ocamposfaria@gmail.com"],
+    "email_on_failure": True,
+    "email_on_success": False,
+    "retries": 0
+}
 
 with DAG(
     dag_id="elt_001_breweries",
@@ -10,6 +22,7 @@ with DAG(
     schedule_interval="0 9 * * *",
     catchup=False,
     tags=["brewery"],
+    default_args=default_args,
 ) as dag:
 
     task_bronze_extract_breweries = PythonOperator(
