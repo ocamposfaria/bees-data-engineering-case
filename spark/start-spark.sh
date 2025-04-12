eee3862bc20c9207dae1c30c96ba1ae17b6849b3
João Pedro Campos Faria <ocamposfaria@gmail.com>
2025-04-12 12:13:42 -0300

    Minor fix

--- spark/start-spark.sh
+++ spark/start-spark.sh
@@ -1,22 +1,26 @@
-#start-spark.sh
 #!/bin/bash
-. "/opt/spark/bin/load-spark-env.sh"
-# When the spark work_load is master run class org.apache.spark.deploy.master.Master
-if [ "$SPARK_WORKLOAD" == "master" ];
-then
 
-export SPARK_MASTER_HOST=`hostname`
+# Comente essa linha se o arquivo não existir
+# . "/opt/spark/bin/load-spark-env.sh"
 
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
+if [ "$SPARK_WORKLOAD" == "master" ]; then
+    export SPARK_MASTER_HOST=$(hostname)
 
-elif [ "$SPARK_WORKLOAD" == "worker" ];
-then
-# When the spark work_load is worker run class org.apache.spark.deploy.master.Worker
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+    cd /opt/spark/bin
+    ./spark-class org.apache.spark.deploy.master.Master \
+        --ip $SPARK_MASTER_HOST \
+        --port $SPARK_MASTER_PORT \
+        --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
 
-elif [ "$SPARK_WORKLOAD" == "submit" ];
-then
+elif [ "$SPARK_WORKLOAD" == "worker" ]; then
+    cd /opt/spark/bin
+    ./spark-class org.apache.spark.deploy.worker.Worker \
+        --webui-port $SPARK_WORKER_WEBUI_PORT \
+        $SPARK_MASTER >> $SPARK_WORKER_LOG
+
+elif [ "$SPARK_WORKLOAD" == "submit" ]; then
     echo "SPARK SUBMIT"
+
 else
     echo "Undefined Workload Type $SPARK_WORKLOAD, must specify: master, worker, submit"
 fi
